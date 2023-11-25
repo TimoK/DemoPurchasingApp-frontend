1d45c485232344fa16fed3b13658ccdd7020f90a
Timo Koppenberg <timokoppenberg@gmail.com>
2023-11-25 14:30:34 +0100

    Fixed failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test for Snakelbaars", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Snakelbaars/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/Components/BuyProcedureCard.jsx
+++ src/Components/BuyProcedureCard.jsx
@@ -2,7 +2,7 @@ import "./BuyProcedureCard.css";
 
 export default function BuyProcedureCard({ title, maxPrice }) {
   return (
-    <div class="buy-procedure-card">
+    <div className="buy-procedure-card">
       <h2>{title}</h2>
       <p className="detail">Max prijs: {maxPrice} euro</p>
     </div>
