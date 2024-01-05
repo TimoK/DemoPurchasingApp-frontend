8cb80db4ac05fa47698ee620156326103e83c5fe
Timo Koppenberg <timokoppenberg@gmail.com>
2024-01-05 14:56:07 +0100

    Formatted card costs

--- src/Components/BuyProcedureCard.jsx
+++ src/Components/BuyProcedureCard.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import "./BuyProcedureCard.css";
+import { euroFormat } from "../format";
 
 export default function BuyProcedureCard({ title, price, id }) {
   return (
     <Link to={id.toString()}>
       <div className="buy-procedure-card">
         <h2>{title ? title : "Nieuwe procedure"}</h2>
-        {price > 0 && <p className="detail">Prijs: {price} euro</p>}
+        {price > 0 && <p className="detail">Inschatting kosten: {euroFormat.format(price)}</p>}
       </div>
     </Link>
   );
