8cf2f06381380fa24e975f8d1163310613410b27
Timo Koppenberg <timokoppenberg@gmail.com>
2024-01-26 08:57:17 +0100

    Removed outdated reference

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainWrapper from "./pages/MainWrapper";
 import BuyProcedureOverview from "./pages/BuyProcedureOverview";
-import BuyProcedurePage from "./pages/BuyProcedurePage";
 import About from "./pages/About";
 import PhasePage from "./pages/PhasePage";
 
