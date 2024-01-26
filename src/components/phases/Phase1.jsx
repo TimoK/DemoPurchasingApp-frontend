3590861892c5cbcc331d47e5aa4d6ddc5b1ebb9f
Timo Koppenberg <timokoppenberg@gmail.com>
2024-01-26 11:18:22 +0100

    Playing around a little with shoelace UI components

--- src/components/phases/Phase1.jsx
+++ src/components/phases/Phase1.jsx
@@ -1,9 +1,11 @@
+import SlButton from "@shoelace-style/shoelace/dist/react/button";
 import TextInput from "../forms/TextInput";
 import InputSection from "../forms/InputSection";
 
 export default function Phase1({ buyProcedure, onChange }) {
   return (
     <InputSection>
+      <SlButton variant="primary">Primary</SlButton>
       <TextInput
         type="text"
         name="title"
--- src/pages/MainWrapper.jsx
+++ src/pages/MainWrapper.jsx
@@ -1,3 +1,8 @@
+import SlButton from "@shoelace-style/shoelace/dist/react/button";
+import SlRadio from "@shoelace-style/shoelace/dist/react/radio";
+import SlRadioGroup from "@shoelace-style/shoelace/dist/react/radio-group";
+import SlInput from "@shoelace-style/shoelace/dist/react/input";
+
 import { useDispatch } from "react-redux";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
@@ -15,6 +20,27 @@ export default function MainWrapper() {
   return (
     <div className="main-div">
       <NavBar />
+      <div className="testshoelace">
+        <SlButton variant="primary" className="shoelace-button">
+          Nieuwe procedure
+        </SlButton>
+
+        <SlRadioGroup
+          label="Binnen welke termijn worden de kosten gemaakt?"
+          name="a"
+          value="1"
+        >
+          <SlRadio value="1">Jaarlijks</SlRadio>
+          <SlRadio value="2">Per kwartaal</SlRadio>
+          <SlRadio value="3">Eenmalig</SlRadio>
+        </SlRadioGroup>
+        <SlButton variant="danger" className="shoelace-button" outline>
+          Verwijder procedure
+        </SlButton>
+
+        <SlInput className="sl-input" label="Wat wil je inkopen?" />
+      </div>
+
       <Outlet />
     </div>
   );
